0c1e2f292bea066103807df348c56afa50bf85a7
LiuKai <3513864951@qq.com>
2025-11-16 20:40:59 +0800

    :sparkles: Initial commit

---
+++ src/main/java/com/club/campusclubmanager/CampusClubManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.club.campusclubmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusClubManagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusClubManagerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/club/campusclubmanager/CampusClubManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.club.campusclubmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusClubManagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
